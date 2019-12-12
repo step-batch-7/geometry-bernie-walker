240184d14721b61b18976d46ffb4d897365c0d78
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2019-12-12 16:05:20 +0530

    added isEqualTo method
    * added a method isEqualTo to the Line class
    
    * allows to check if the line is equals to the other line.

--- src/line.js
+++ src/line.js
@@ -7,6 +7,15 @@ class Line {
   toString() {
     return `two points of the line are A = (${this.A}) and B = (${this.B})`;
   }
+
+  isEqualTo(anotherLine) {
+    const isPointOneEqual =
+      this.A[0] == anotherLine.A[0] && this.A[1] == anotherLine.A[1];
+    const isPointTwoEqual =
+      this.B[0] == anotherLine.B[0] && this.B[1] == anotherLine.B[1];
+
+    return isPointOneEqual && isPointTwoEqual;
+  }
 }
 
 module.exports = Line;
--- test/test_line.js
+++ test/test_line.js
@@ -3,12 +3,29 @@ const Line = require("../src/line");
 
 describe("line", function() {
   const myLine = new Line([2, 3], [3, 4]);
+  const similarLine = new Line([2, 3], [3, 4]);
+  const nonSimilarLine = new Line([1, 1], [1, 2]);
 
   describe("toString", function() {
     it("should display the string representation of the line", function() {
       const actual = myLine.toString();
       const expected = `two points of the line are A = (2,3) and B = (3,4)`;
       assert.strictEqual(actual, expected);
+    });
+  });
+
+  describe("isEqualTo", function() {
+    const similarLine = new Line([2, 3], [3, 4]);
+    const nonSimilarLine = new Line([1, 1], [1, 2]);
+
+    it("should accept the equality of similar lines", function() {
+      const actual = myLine.isEqualTo(similarLine);
+      assert.isTrue(actual);
+    });
+
+    it("should refuse the equality of non similar lines", function() {
+      const actual = myLine.isEqualTo(nonSimilarLine);
+      assert.isFalse(actual);
     });
   });
 });
