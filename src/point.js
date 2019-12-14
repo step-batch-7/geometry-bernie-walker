8068ddb0975293b2cc42a99fd35286ca142b6c75
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2019-12-14 12:57:47 +0530

    added a method visit to Point
    * added a methos visit to the Point class.
    
    * this method accepts a function reference which accepts two parameters
    i.e. the coordinates of the point and performs the specific opetation.

--- src/point.js
+++ src/point.js
@@ -7,6 +7,10 @@ class Point {
   toString() {
     return `[Point @(${this.x},${this.y})]`;
   }
+
+  visit(functionReference) {
+    return functionReference(this.x, this.y);
+  }
 }
 
 module.exports = Point;
--- test/test_point.js
+++ test/test_point.js
@@ -16,4 +16,20 @@ describe("point", function() {
       assert.strictEqual(actual, expected);
     });
   });
+
+  describe("visit", function() {
+    const point = new Point(2, 3);
+    const mul = (a, b) => a * b;
+    const add = (a, b) => a + b;
+
+    it("should visit the mul function and return the product", function() {
+      const actual = point.visit(mul);
+      assert.strictEqual(actual, 6);
+    });
+
+    it("should visit the add function and return the sum", function() {
+      const actual = point.visit(add);
+      assert.strictEqual(actual, 5);
+    });
+  });
 });
